bd72c84d67df1b30de092790562a13bd85addc57
haruto8610 <haruto.fukushina0207@gmail.com>
2025-12-08 11:47:18 +0900

    アラート

--- app/phone/app/index.tsx
+++ app/phone/app/index.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
-    return (
-      <View className="flex-1 justify-center items-center bg-sky-50">
-        <Text className="text-3xl font-bold">Home Screen</Text>
-      </View>
-    );
-  }
-  
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold">Home Screen</Text>
+      <Link href="/home" className="bg-green-500 px-8 py-4 rounded-xl">
+        <Text className="text-white font-bold text-lg">設定ページへ</Text>
+      </Link>
+    </SafeAreaView>
+  );
+}
